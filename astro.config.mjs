1909eb55ffa9a84625ebab4ecb73ed1a17e40747
Maru <ta96sse@gmail.com>
2026-01-28 21:17:19 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    // 【重要】独自ドメインを設定
+    site: 'https://mori-mori.tokyo',
+    // リポジトリ名を含まないトップレベルドメインなので base は '/' でOK（デフォルト）
+    base: '/',
     integrations: [tailwind()],
     build: {
         assets: 'assets'
